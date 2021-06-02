3b9800c633dfa495e376a667e6ce15b5cc179551
Safia <69358550+fi-ya@users.noreply.github.com>
2021-06-02 16:24:12 +0100

    L to uppercase

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Footer from "../components/Footer";
-import Layout, { siteTitle } from "../components/layout";
+import Layout, { siteTitle } from "../components/Layout";
 import Link from "next/link";
 
 export default function Home() {
