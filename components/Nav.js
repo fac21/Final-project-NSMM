bfa98bb14de743e4bc1e614d19668576cfe44e2b
mikailbadoula <michaeldevfay@gmail.com>
2021-06-07 17:31:51 +0100

    stick footer to bottom of page

--- components/Nav.js
+++ components/Nav.js
@@ -1,54 +1,54 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Nav() {
-    return (
-        <div className={styles.nav}>
-            <Link href='/events'>
-                <a>
-                    <Image
-                    src='/images/home-alt.svg'
-                    className=''
-                    alt='Home'
-                    width={24}
-                    height={24}
-                    />
-                </a>
-            </Link>
-            <Link href='/chat'>
-            <a>
-                    <Image
-                    src='/images/chat.svg'
-                    className=''
-                    alt='Chat'
-                    width={24}
-                    height={24}
-                    />
-                </a>
-            </Link>
-            <Link href='/profile'>
-            <a>
-                    <Image
-                    src='/images/user.svg'
-                    className=''
-                    alt='Profile'
-                    width={24}
-                    height={24}
-                    />
-                </a>
-            </Link>
-            <Link href='/settings'>
-            <a>
-                    <Image
-                    src='/images/settings.svg'
-                    className=''
-                    alt='Settings'
-                    width={24}
-                    height={24}
-                    />
-                </a>
-            </Link>
-        </div>
-    )
+  return (
+    <div className={styles.nav}>
+      <Link href="/events">
+        <a>
+          <Image
+            src="/images/home-alt.svg"
+            className=""
+            alt="Home"
+            width={24}
+            height={24}
+          />
+        </a>
+      </Link>
+      <Link href="/chat">
+        <a>
+          <Image
+            src="/images/chat.svg"
+            className=""
+            alt="Chat"
+            width={24}
+            height={24}
+          />
+        </a>
+      </Link>
+      <Link href="/profile">
+        <a>
+          <Image
+            src="/images/user.svg"
+            className=""
+            alt="Profile"
+            width={24}
+            height={24}
+          />
+        </a>
+      </Link>
+      <Link href="/settings">
+        <a>
+          <Image
+            src="/images/settings.svg"
+            className=""
+            alt="Settings"
+            width={24}
+            height={24}
+          />
+        </a>
+      </Link>
+    </div>
+  );
 }
