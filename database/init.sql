e54d5d4b43c592a0bceae4ecf7d8527edd5fc14e
Mariya Peychinova <mariapeichinova@gmail.com>
2021-06-07 16:26:13 +0100

    change date format
    Co-authored-by: Safia Ali <safiaali@hotmail.com>
    Co-authored-by: Michael Devlin <23028288+michael-devlin@users.noreply.github.com>

--- database/init.sql
+++ database/init.sql
@@ -111,8 +111,8 @@ INSERT INTO interests(interest_name, interest_icon) VALUES
 ;
 
 INSERT INTO events(user_id, interests_id, event_title, event_description, location, date, time) VALUES
-(1, 2, 'Anyone free for coffee, tues 8th June, 6pm?', 'I will be in Finsbury Park and will have a couple if hours free. Would love to meet up for coffee. Please reply below!', 'London', '08-06-2021', 'afternoon'),
-(2, 1, 'Drinks this weekend, central London', 'Anyone wanna go for a drink this weekend in central London please let me know below!', 'London', '09-06-2021', 'evening')
+(1, 2, 'Anyone free for coffee, tues 8th June, 6pm?', 'I will be in Finsbury Park and will have a couple if hours free. Would love to meet up for coffee. Please reply below!', 'London', '2021-06-08', 'afternoon'),
+(2, 1, 'Drinks this weekend, central London', 'Anyone wanna go for a drink this weekend in central London please let me know below!', 'London', '2021-06-09', 'evening')
 ;
 
 INSERT INTO event_response (user_id, response_content, event_id) VALUES
@@ -120,7 +120,7 @@ INSERT INTO event_response (user_id, response_content, event_id) VALUES
 (1, 'Looking forward to having a beer after a long week – see you there!', 2)
 ;
 
-INSERT INTO user_profiles(user_id, username, password, dob, gender, interests_id, location, bio) VALUES (1, 'chumUser1', 'chumUser1', '01-01-1980', 'male', (ARRAY[1,2]), 'London', 'I like coding and would like to meet for coffee or a pint')
+INSERT INTO user_profiles(user_id, username, password, dob, gender, interests_id, location, bio) VALUES (1, 'chumUser1', 'chumUser1', '1980-01-01', 'male', (ARRAY[1,2]), 'London', 'I like coding and would like to meet for coffee or a pint')
 ;
 
 
